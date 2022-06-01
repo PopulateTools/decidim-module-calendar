f30a935be50cb8010930a475a9c62123b5462851
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-06-01 13:58:28 +0200

    Disable calendar module as participatory space

--- lib/decidim/calendar.rb
+++ lib/decidim/calendar.rb
@@ -3,7 +3,6 @@
 require "decidim/event_calendar/admin"
 require "decidim/event_calendar/admin_engine"
 require "decidim/event_calendar/engine"
-require "decidim/event_calendar/participatory_space"
 
 module Decidim
   module EventCalendar
